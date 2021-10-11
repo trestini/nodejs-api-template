0a95fb4f3790774f49b63571004eddf9bcaf67c1
Rafael Trestini <trestini@gmail.com>
2021-10-10 21:05:03 -0300

    fix: configuration defaults

--- src/modules/feature-api.ts
+++ src/modules/feature-api.ts
@@ -12,28 +12,52 @@ import { Next, Context } from "koa";
 import { RouteMapper } from "../utils/route-mapper";
 import Bootable from "../utils/bootable";
 import ConfigHandler from "../utils/config-handler";
+import MongoDB from "./mongodb";
+
+import {readdirSync} from 'fs';
 
 export default class FeatureApi implements Bootable {
 
   config: ConfigHandler;
+  koaApp: Koa = new Koa();
+  mongodb: MongoDB;
 
   async boot(){
+    const PORT = this.config.contents.PORT || process.env.PORT || process.env.port || 3000;
+
+    const { 
+      autoDiscoveryFeatures, 
+      autoDiscoveryFeaturesBasePath, 
+      routes} = this.config.contents.featureApi || {};
+    
     return new Promise<any>((resolve, reject) => {
-      const app = new Koa();
       const rootRouter = new Router();
-      
-      const PORT = this.config.contents.PORT || process.env.PORT || process.env.port || 3000;
-      
       const routeMapper = new RouteMapper(rootRouter);
+
+      if( autoDiscoveryFeatures ) {
+        const featuresBasePath = autoDiscoveryFeaturesBasePath || './src/features';
+        readdirSync(featuresBasePath, {withFileTypes: true})
+          .filter(e => e.isDirectory())
+          .map(d => d.name)
+          .forEach(feature => {
+            console.log(`[Feature API] Auto adding feature '${feature}' on path '/${feature}'`);
+            routeMapper.addRouter(`/${feature}`, feature)
+          });
+      }
       
-      this.config.contents.routes && this.config.contents.routes.forEach(route => {
+      routes && routes.forEach(route => {
+        console.log(`[Feature API] Adding route '${route.name}' on path '${route.path}'`);
         routeMapper.addRouter(route.path, route.name);
       });
       
-      app
+      this.koaApp
         .use(cors())
         .use(async (ctx: Context, next: Next) => {
           ctx.config = this.config.contents;
+          await next();
+        })
+        .use(async (ctx: Context, next: Next) => {
+          ctx.mongodb = this.mongodb;
           await next();
         })
         .use(favicon())
@@ -44,11 +68,11 @@ export default class FeatureApi implements Bootable {
         .use(rootRouter.allowedMethods());
       
       try {
-        app.on('error', (e) => {
+        this.koaApp.on('error', (e) => {
           reject(e);
         });
         
-        const server = app.listen(PORT);
+        const server = this.koaApp.listen(PORT);
         server.on('error', (e) => {
           reject(e);
         });
